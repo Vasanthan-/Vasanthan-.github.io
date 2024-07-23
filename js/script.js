f810d43f699b3d485ee7bc4e1c54469ba24bf993
vasanthan- <vasanthancarbon@gmail.com>
2024-07-23 20:31:08 +0530

    css update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function coffeecup() {
   alert("You clicked the coffee cup!");
 }
+
+window.onscroll = () => {
+  document.querySelector(".mountains").style.marginTop = scrollY/2 + "px";
+  document.querySelector(".stars").style.marginBottom = scrollY/2.5 + "px";
+  document.querySelector(".stars").style.marginLeft = scrollY/2 + "px";
+  document.querySelector(".cave").style.transform = "scale(" + (scrollY/5000 + 1) + ")";
+  document.querySelector(".moon").style.transform = "rotate(-" + (scrollY/5.5) + "deg)";
+}
